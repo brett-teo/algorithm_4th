622cd5dad69250b12d7748a6354dbe91f8616b80
wuweiping <wuweipingface@gmail.com>
2022-03-23 23:59:00 +0800

    TODO: Complete the Iterator for LinkedList

--- linkNodeImplementBag.js
+++ linkNodeImplementBag.js
@@ -6,6 +6,7 @@ class Node {
 class Bag {
   first = null
   size = 0
+  current = null
   add(item) {
     const oldFirst = this.first
     this.first = new Node()
@@ -20,4 +21,37 @@ class Bag {
   getSize() {
     return this.size
   }
+  [Symbol.iterator]() {
+    return this
+  }
+
+  hasNext() {
+    return !!this.current.next
+  }
+
+  next() {
+    if (!this.current) {
+      this.current = this.first
+    }
+    const item = this.current.item
+    const hasNext = this.hasNext()
+    this.current = this.current.next
+    return {
+      done: !hasNext,
+      value: item
+    }
+  }
+}
+
+const bag = new Bag()
+bag.add('to')
+bag.add('be')
+// bag.add('or')
+// bag.add('not')
+// bag.add('to')
+// bag.add('be')
+console.log('bag', bag)
+
+for (const item of bag) {
+  console.log('item', item)
 }
